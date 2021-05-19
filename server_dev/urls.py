e6338fe49623d674550d755d1de9f84dad6dea63
sanghak <eaa0305@naver.com>
2021-05-20 03:15:11 +0900

    no message

--- server_dev/urls.py
+++ server_dev/urls.py
@@ -1,16 +1,21 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import url, include
-from rest_framework.routers import DefaultRouter
+"""server_dev URL Configuration
 
-from . import views
-
-router = DefaultRouter()
-router.register(r'pins', views.ContactVerificationViewSet)
-router.register(r'contacts', views.ContactViewSet)
-# router.register(r'countries', views.CountryViewSet, base_name='countries')  # base_name 더이상 사용 안함
-router.register(r'countries', views.CountryViewSet, basename='countries')  # 최신버전 리눅스 2.9.7
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    # url(r'^countries/$', views.CountryList.as_view(), name="country-list"),
+    path('admin/', admin.site.urls),
 ]
