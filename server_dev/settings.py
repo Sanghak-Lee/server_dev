26e40b8dd12a0313651d847044a3c3bbde9b15b2
sanghak <eaa0305@naver.com>
2021-05-19 15:26:59 +0900

    python manage.py migrate

--- server_dev/settings.py
+++ server_dev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bn6h+1i^@y%&-y3zgy544zcxk6dp^^5nno58dy2xjqtc%7p4$6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
